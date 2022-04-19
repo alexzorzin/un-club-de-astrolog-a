d881a4be851c1ec36d5999c16f7f755e6dbc2899
Lucas Macias <88591296+l-macias@users.noreply.github.com>
2022-04-19 19:27:41 +0000

    Modal añadido con form funcionando
    Se puso estático el nav y se corrigieron algunas cosas. Fuente por defecto la que corresponde.
    
    Faltan añadir algunos estilos al formulario

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//NAV Menu Burger
 document.addEventListener('DOMContentLoaded', nav)
 function nav(){
     const burger = document.querySelector('.burger');
@@ -6,3 +7,14 @@ function nav(){
         nav.classList.toggle('show')
     })
 }
+
+//Modal
+const btnEnviar = document.getElementById('confirmar-enviar')
+
+let mensajeEnviar = document.getElementById('mensaje-enviar')
+    btnEnviar.addEventListener("click",(e)=>{
+      
+      mensajeEnviar.className = 'mensaje-enviado'
+    })
+    mensajeEnviar.className = 'mensaje-enviar'
+  
