c50b5b044abe55e59ce1ed733e8034871cafee12
Lucas Macias <88591296+l-macias@users.noreply.github.com>
2022-04-22 04:06:22 +0000

    Modificado el comportamiento del nav
    Lo modifiqué de cero. Salió bien y sin usar jquery jaja

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 //NAV Menu Burger
-document.addEventListener('DOMContentLoaded', nav)
-function nav(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.main-nav');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
-}
+const nav = document.querySelector(".main-nav");
+window.addEventListener("click", function (e) {
+    if (document.querySelector(".burger").contains(e.target)) {
+        nav.classList.toggle("show");
+    } else if (nav.classList.value.includes("show")) {
+        nav.classList.toggle("show");
+    }
+});
 
 //Modal
-const btnEnviar = document.getElementById('confirmar-enviar')
+const btnEnviar = document.getElementById("confirmar-enviar");
 
-let mensajeEnviar = document.getElementById('mensaje-enviar')
-    btnEnviar.addEventListener("click",(e)=>{
-      
-      mensajeEnviar.className = 'mensaje-enviado'
-    })
-    mensajeEnviar.className = 'mensaje-enviar'
-  
+let mensajeEnviar = document.getElementById("mensaje-enviar");
+btnEnviar.addEventListener("click", (e) => {
+    mensajeEnviar.className = "mensaje-enviado";
+});
+mensajeEnviar.className = "mensaje-enviar";
